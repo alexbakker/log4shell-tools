e881d75508b7819506be66fe763c1450cb91b942
Alexander Bakker <ab@alexbakker.me>
2022-05-22 14:12:31 +0200

    Switch to unstable nixpkgs channel and use flake-utils

--- flake.nix
+++ flake.nix
@@ -1,34 +1,36 @@
 {
   description = "Nix flake for log4shell-tools";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }: let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-    in {
-      defaultPackage.x86_64-linux =
-        with pkgs; buildGoModule {
-          pname = "log4shell-tools";
-          version = "0.0.0";
-          src = ./.;
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        defaultPackage =
+          with pkgs; buildGoModule {
+            pname = "log4shell-tools";
+            version = "0.0.0";
+            src = ./.;
 
-          vendorSha256 = "sha256-eaS8wseZVYKSx60zP7Z0R3j2IzkfFQU7JhfMDEND+IA=";
+            vendorSha256 = "sha256-eaS8wseZVYKSx60zP7Z0R3j2IzkfFQU7JhfMDEND+IA=";
 
-          doCheck = false;
-
-          subPackages = [ "./cmd/log4shell-tools-server" ];
+            subPackages = [ "./cmd/log4shell-tools-server" ];
+          };
+        nixosConfigurations.devContainer = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./container.nix
+          ];
         };
-      nixosConfigurations.devContainer = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./container.nix
-        ];
-      };
-      devShell.x86_64-linux = with pkgs; mkShell {
-        buildInputs = [
-          go
-          maven
-          openjdk8
-        ];
-      };
-    };
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            go
+            maven
+            openjdk8
+          ];
+        };
+      }
+    );
 }
